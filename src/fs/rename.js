5205fca324d46e4efa4e13f18d8562188dd8702a
Katsiaryna Piatrova <katsiaryna.piatrova@refinitiv.com>
2022-11-26 14:18:58 +0400

    implement rename.js

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,16 @@
+import fs from 'fs';
 const rename = async () => {
-    // Write your code here 
+  const src = 'src/fs/files/wrongFilename.txt';
+  const dest = 'src/fs/files/properFilename.md';
+  if (fs.existsSync(dest) || !fs.existsSync(src)) {
+    throw new Error(
+      'TFS operation failed (File already exists or source folder doesnt exist)'
+    );
+  } else {
+    fs.rename(src, dest, () => {
+      console.info('\x1b[42m', '\nFile Renamed!\n');
+    });
+  }
 };
 
 await rename();
