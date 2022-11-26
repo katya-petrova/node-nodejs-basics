dc0c6c001ab6f4c02fdf094a29c8566700a963d2
Katsiaryna Piatrova <katsiaryna.piatrova@refinitiv.com>
2022-11-26 13:56:13 +0400

    implement copy.js

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,18 @@
+import fs from 'fs';
 const copy = async () => {
-    // Write your code here 
+  const src = `src/fs/files`;
+  const dest = `src/fs/files_copy`;
+  if (fs.existsSync(dest) || !fs.existsSync(src)) {
+    throw new Error(
+      'TFS operation failed (File already exists or source folder doent exist)'
+    );
+  } else {
+    fs.cp(src, dest, { recursive: true }, (err) => {
+      if (err) {
+        console.error(err);
+      }
+    });
+  }
 };
 
 copy();
