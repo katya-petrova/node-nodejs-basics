ffb165657427fa88b3cb9037ccf03d8dd4c4e86e
Katsiaryna Piatrova <katsiaryna.piatrova@refinitiv.com>
2022-11-26 13:07:25 +0400

    implement create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,17 @@
+import fs from 'fs';
+import { writeFile } from 'fs/promises';
+
 const create = async () => {
-    // Write your code here 
+  try {
+    const path = 'src/fs/files/fresh.txt';
+    if (fs.existsSync(path)) {
+      throw new Error('TFS operation failed');
+    } else {
+      await writeFile(path, 'I am fresh and young');
+    }
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 await create();
