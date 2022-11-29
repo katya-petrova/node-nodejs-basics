80df9f4f6ff7e178652ef9fc327388e15f2d2904
Katsiaryna Piatrova <katsiaryna.piatrova@refinitiv.com>
2022-11-29 19:51:26 +0400

    implement args.js

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,7 @@
 const parseArgs = () => {
-    // Write your code here 
+  const args = process.argv.slice(2);
+
+  console.log(`${args[0]} is ${args[1]}, ${args[2]} is ${args[3]}`);
 };
 
 parseArgs();
