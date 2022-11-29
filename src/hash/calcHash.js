377440a234d96200efe835b2109c18b5e7c2e391
Katsiaryna Piatrova <katsiaryna.piatrova@refinitiv.com>
2022-11-29 21:13:41 +0400

    implement calcHash.js

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,18 @@
+import fs from 'fs';
+import { createHash } from 'crypto';
 const calculateHash = async () => {
-    // Write your code here 
+  const src = 'src/hash/files/fileToCalculateHashFor.txt';
+  if (!fs.existsSync(src)) {
+    throw new Error(`TFS operation failed (File doesn't exist)`);
+  } else {
+    fs.readFile(src, 'utf8', (err, data) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      console.log(createHash('sha256').update(data).digest('hex'));
+    });
+  }
 };
 
 await calculateHash();
