557ec905e56d955404c9dcd6f52f3325723b08b4
Katsiaryna Piatrova <katsiaryna.piatrova@refinitiv.com>
2022-11-29 21:56:35 +0400

    refactor args.js

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,7 +1,17 @@
 const parseArgs = () => {
   const args = process.argv.slice(2);
+  let arr = [];
 
-  console.log(`${args[0]} is ${args[1]}, ${args[2]} is ${args[3]}`);
+  args.map((el, i) => {
+    if (i % 2 === 0) {
+      arr.push(el.slice(2));
+    } else {
+      arr.push(el);
+    }
+  });
+  console.log(
+    `${arr[0]} is ${arr[1]}, ${arr[2]} is ${args[3]}, ${arr[4]} is ${arr[5]} `
+  );
 };
 
 parseArgs();
