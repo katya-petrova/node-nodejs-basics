fcff832b3edaf2a33c333d717030298c59c14347
Katsiaryna Piatrova <katsiaryna.piatrova@refinitiv.com>
2022-12-02 16:42:19 +0400

    implement decompress.js

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,13 @@
+import zlib from 'zlib';
+import fs from 'fs';
 const decompress = async () => {
-    // Write your code here 
+  var unzip = zlib.createUnzip();
+
+  var read = fs.createReadStream('src/zip/files/archive.gz');
+  var write = fs.createWriteStream('src/zip/files/fileToCompress.txt');
+
+  read.pipe(unzip).pipe(write);
+  console.log('unZipped Successfully');
 };
 
 await decompress();
