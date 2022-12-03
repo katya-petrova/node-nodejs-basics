07169b2e019b1c5d1e9e2233de59d921f23f6eaf
Katsiaryna Piatrova <katsiaryna.piatrova@refinitiv.com>
2022-12-03 17:34:08 +0400

    implement main.js

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,45 @@
-const performCalculations = async () => {
-    // Write your code here
-};
+import { Worker, isMainThread } from 'node:worker_threads';
 
+import { cpus } from 'os';
+
+const performCalculations = async () => {
+  let primes = [];
+  const systemCpuCores = cpus();
+  if (isMainThread) {
+    const threadCount = systemCpuCores.length;
+    const threads = new Set();
+    // console.log(`Running with ${threadCount} threads...`);
+    let n = 10;
+    for (let i = 0; i < threadCount - 1; i++) {
+      threads.add(
+        new Worker('./src/wt/worker.js', {
+          workerData: { n: n++, order: n },
+        })
+      );
+    }
+    for (let worker of threads) {
+      worker.on('error', (err) => {
+        primes.push({ status: 'error', data: null });
+      });
+      worker.on('exit', () => {
+        threads.delete(worker);
+        // console.log(`Thread exiting, ${threads.size} running...`);
+        if (threads.size === 0) {
+          console.log(sortArr(primes));
+        }
+      });
+      worker.on('message', (msg) => {
+        primes.push(msg);
+      });
+    }
+  }
+};
 await performCalculations();
+
+const sortArr = (arr) => {
+  let sortedArr = arr.sort((a, b) => a.order - b.order);
+
+  sortedArr.map((el) => delete el.order);
+
+  return sortedArr;
+};
